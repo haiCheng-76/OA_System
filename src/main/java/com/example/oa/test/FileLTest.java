7b2a20025f905112ea4e6c06603b35a2a533a502
lhc <582895699@qq.com>
2018-03-13 14:30:06 +0800

    completed file test

---
+++ src/main/java/com/example/oa/test/FileLTest.java
@@ -0,0 +1,27 @@
+package com.example.oa.test;
+
+import static org.junit.Assert.*;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import com.example.oa.domain.File;
+import com.example.oa.service.FileService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath*:root-context.xml",
+"classpath*:servlet-context.xml" })
+public class FileLTest {
+	@Autowired
+	private FileService fileService;
+
+	@Test
+	public void testGetallFileInfo() {
+		List<File> listfile = fileService.getallFile();
+		System.out.println(listfile.size());
+		assertEquals(5, listfile.size());
+	}
+
+}
