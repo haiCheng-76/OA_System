4e8b5fd07e26234bd57af7f96407b1d33fdc1143
lhc <582895699@qq.com>
2018-03-13 10:35:43 +0800

    update readme

--- src/main/java/com/example/oa/control/EmployeeControl.java
+++ src/main/java/com/example/oa/control/EmployeeControl.java
@@ -1,15 +1,10 @@
 package com.example.oa.control;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.example.oa.domain.Employee;
 import com.example.oa.service.EmployeeService;
 
--- src/main/java/com/example/oa/control/FileControl.java
+++ src/main/java/com/example/oa/control/FileControl.java
@@ -2,7 +2,6 @@ package com.example.oa.control;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
